b903ca6a6bc82b83bf23cfa7afe4cb3dc992384b
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-02-20 11:49:53 -0300

    feat: using `fetch` to list git repos

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository?.name ?? 'Default'}</strong>
-      <p>{props.repository?.description ?? 'description'}</p>
-      <a href={props.repository?.url ?? '#'}>more...</a>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository?.description ?? 'oops.. no description'}</p>
+      <a href={props.repository.html_url }>see more...</a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'Gabriel',
-  description: 'my repository',
-  url: 'https://github.com/gabrielrochas',
-};
+const url = 'https://api.github.com/users/gabrielrochas/repos';
 
 export function RepositoryList() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setRepos(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(repos);
+
   return (
     <section className='repository-list'>
       <ul>
-        <RepositoryItem repository={repository} />
+        {repos.map((repos) => (
+          <RepositoryItem repository={repos} key={repos.id}/>
+        ))}
       </ul>
     </section>
   );
