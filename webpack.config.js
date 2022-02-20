e96e4b0dcba2cd51438574e097f8563c55a4f071
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-02-20 11:20:49 -0300

    chore!: add react-refresh-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
@@ -8,6 +8,7 @@ module.exports = {
   devtool: isDevelopment? 'eval-source-map' : 'source-map',
   devServer: {
     static: path.resolve(__dirname, 'public'),
+    hot: true,
   },
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
@@ -18,16 +19,24 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean),
+          }
+        },
       },
       {
         test: /\.scss$/,
