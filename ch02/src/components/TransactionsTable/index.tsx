e2e58b93dc0bc74ff1e783ebced95f5d58889524
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-02-28 11:10:17 -0300

    feat!: consuming data from api

--- ch02/src/components/TransactionsTable/index.tsx
+++ ch02/src/components/TransactionsTable/index.tsx
@@ -1,12 +1,25 @@
-import { useEffect } from "react";
-import { api } from "../../services/api";
-import { Container } from "./styles";
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles';
+
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  amount: number;
+  category: string;
+  createdAt: string;
+}
 
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get('transactions')
-      .then((response) => console.log(response.data));
-  }, [])  
+    api
+      .get('transactions')
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -19,18 +32,23 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>fasçdfj</td>
-            <td className="deposit">R$ 12,00</td>
-            <td>Desdd</td>
-            <td>20/02/21</td>
-          </tr>
-          <tr>
-            <td>fasçdfj</td>
-            <td className="withdraw">-R$ 12,00</td>
-            <td>Desdd</td>
-            <td>20/02/21</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- ch02/src/index.tsx
+++ ch02/src/index.tsx
@@ -5,20 +5,41 @@ import { createServer, Model } from 'miragejs';
 
 createServer({
   models: {
-    transaction: Model
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Ailta app',
+          type: 'deposit',
+          category: 'Job',
+          amount: 3000,
+          createdAt: new Date('2022-02-02 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Ailta app - new sale',
+          type: 'deposit',
+          category: 'Job',
+          amount: 4000,
+          createdAt: new Date('2022-01-31 10:00:00'),
+        },
+      ],
+    });
   },
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return [
-        this.schema.all('transaction')
-      ];
+      return [this.schema.all('transaction')];
     });
     this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
+      const data = JSON.parse(request.requestBody);
 
       return schema.create('transaction', data);
-    })
+    });
   },
 });
 
