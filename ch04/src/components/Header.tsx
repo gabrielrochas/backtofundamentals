2df42f170ca6094969663095a6d58fcd420532d5
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-05-06 20:49:59 -0300

    feat: implement user details to header

--- ch04/src/components/Header.tsx
+++ ch04/src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from 'react-icons/ri';
 
 export default function Header() {
   return (
@@ -30,11 +34,48 @@ export default function Header() {
         alignSelf="center"
         color="gray.200"
         position="relative"
-        bg='gray.800'
+        bg="gray.800"
         borderRadius="full"
       >
-        <Input color='gray.50' variant='unstyled' px="4" mr='4' placeholder="Search..." _placeholder={{ color: 'gray.400'}}/>
-        <Icon as={RiSearchLine} fontSize='20'/>
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Search..."
+          _placeholder={{ color: 'gray.400' }}
+        />
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Gabriel Rocha</Text>
+
+            <Text color="gray.300" fontSize="small">
+              gabriel.rochaas@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            name="Gabriel Rocha"
+            src="https://github.com/gabrielrochas.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
