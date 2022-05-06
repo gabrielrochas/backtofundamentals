de50a2bb05871ad92da293acd7555a4d1bdcbef0
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-05-05 21:12:08 -0300

    feat(Login): implement login form

--- ch04/src/pages/index.tsx
+++ ch04/src/pages/index.tsx
@@ -1,10 +1,55 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <Flex>
-      <h1>dashgo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg={'gray.800'}
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              name="email"
+              id='email'
+              type="email"
+              border="none"
+              bg="gray.900"
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              id='passw'
+              type="password"
+              border="none"
+              bg="gray.900"
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="linkedin">
+          Sign in
+        </Button>
+      </Flex>
     </Flex>
   );
 };
